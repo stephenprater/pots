53d19ac356caa390c4008cacc7e5c6f21a9a3307
A.G. Russell Knives <stephenp@agrussell.com>
2012-09-06 17:03:30 -0500

    work work work

--- app/inputs/autocomplete_association_input.rb
+++ app/inputs/autocomplete_association_input.rb
@@ -1,7 +1,8 @@
 class AutocompleteAssociationInput < SimpleForm::Inputs::CollectionInput
+  enable :placeholder
+  
   def input
     raise ArgumentError, "Autocomplete only works with associations" if reflection.nil?
-    options.delete :as  #seriously, why is this still here?
 
     @url     = options.delete :url
     @field   = options.delete :autocomplete_on || "value"
@@ -18,9 +19,8 @@ class AutocompleteAssociationInput < SimpleForm::Inputs::CollectionInput
     output = String.new.html_safe
     
     if reflection.collection?
-      object.send(method).empty? and object.send(association_name).new
+      object.send(association_name).empty? and object.send(association_name).new
     else
-      binding.pry
       object.send("#{association_name}=", (object.send(association_name) || association_class.new))
     end
     
@@ -31,16 +31,14 @@ class AutocompleteAssociationInput < SimpleForm::Inputs::CollectionInput
         output += f.check_box_field :_delete
       else
         output += f.hidden_field :id 
-        id_element = output.match(/id="(.*?)"/)[1]
-        options.reverse_merge!({
+        id_element = "#" + output.match(/id="(.*?)"/)[1]
+        tag_options = { 
           :"data-autocomplete" => @url,
           :"data-id-element" => id_element,
-          :"data-association" => association_name})
-
-        tag_options =  {}
-        tag_options = options.merge(input_html_options)
-
-        output += template.text_field_tag "search_#{association_name}", nil, tag_options
+          :"data-association" => association_name
+        }
+        
+        output += template.text_field_tag("search_#{association_name}", nil, input_html_options.merge(tag_options))
       end
     end
 
